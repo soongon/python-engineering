cec0748f90e958b306cdb110183bbe75d2e325fd
soongon <soongon@hucloud.co.kr>
2021-06-10 11:25:41 +0900

    final day

---
+++ mongodb_works/collection_type.py
@@ -0,0 +1,17 @@
+# 컬렉션 --> list, dict (dictionary)
+
+# list samples
+favorite_numbers = [1, 2, 3, 4, 5, 6]
+favorite_colors = ['red', 'yellow', 'tan']
+favorite_movies = [
+    ['강재규', '1995', '쉬리'],
+    ['봉준호', '2020', '기생충'],
+] # list of list
+
+# dict sample
+movies = {
+    '강재규': '쉬리',
+    '봉준호': '기생충'
+}
+
+print(movies['봉준호'])
--- mongodb_works/connection_test.py
+++ mongodb_works/connection_test.py
@@ -1,6 +1,15 @@
 import pymongo
+import pprint
 
 conn = pymongo.MongoClient(
-    "mongodb+srv://admin:1234qwer@cluster0.9lxgd.mongodb.net/python_test?retryWrites=true&w=majority")
+    "mongodb+srv://admin:1234qwer@cluster0.9lxgd.mongodb.net/?retryWrites=true&w=majority")
 
-print(conn)
+comments = conn.sample_mflix.comments
+
+# 데이터 조회 (find)
+result = list(comments.find({
+    'name': 'Andrea Le'
+}))
+
+pprint.pprint(result[1]['email'])
+conn.close()
